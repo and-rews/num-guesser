b9ad262306375f7e5685b685acbe11ad13b58bd6
Andrews <57878892+and-rews@users.noreply.github.com>
2021-07-21 21:52:06 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,102 @@
+/*
+- Player must guess a number between a min and max
+- Player gets a certain amount of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer
+- Let player choose to play again
+*/
+
+// Game Values
+let min = 1,
+    max = 10,
+    winningNum = getRandomNum(min, max),
+    guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Play Again event listener
+game.addEventListener('mousedown', function(e) {
+  if(e.target.className === 'play-again') {
+    window.location.reload();
+  }
+})
+
+// Listen for guess
+guessBtn.addEventListener('click', function() {
+  let guess = parseInt(guessInput.value);
+
+
+  // Validate
+  if(isNaN(guess) || guess > max || guess < min) {
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  }
+
+  // Check if won
+  if(guess === winningNum) {
+    // Game over - Won
+    
+    gameOver(true, `${winningNum} is correct, YOU WIN!`);
+  } else {
+    // Wrong Number
+    guessesLeft -= 1;
+
+    if(guessesLeft === 0) {
+      // Game Over - Lost
+
+      gameOver(false, `Game over, you lost. The correct number was ${winningNum}`);
+    } else {
+      // Game Continue - answer wrong
+
+      // Change border color
+      guessInput.style.borderColor = 'red';
+
+      // Clear input
+      guessInput.value = '';
+
+      // Tell User it's wrong number
+      setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+    }
+
+  }
+});
+
+// Game Over
+function gameOver(won, msg) {
+  let color;
+  won === true ? color = 'green' : color = 'red'
+  // Disable input
+  guessInput.disabled = true;
+  // Change border color
+  guessInput.style.borderColor = color;
+  // Set text color
+  message.style.color = color;
+  // Set Message
+  setMessage(msg);
+
+  // Play again
+  guessBtn.value = 'Play Again';
+  guessBtn.className += 'play-again';
+}
+
+// Get Winning Number
+function getRandomNum(min, max) {
+ return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Set Message
+
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
+
